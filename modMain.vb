4d159f492f505e2fb1d5e1d4957da067c7a54569
pedrogon88 <timbo.pgonzalez@gmail.com>
2021-12-23 09:28:23 +0100

    ...

--- modMain.vb
+++ modMain.vb
@@ -2,7 +2,7 @@
 Imports System.Threading
 Imports Newtonsoft.Json.Linq
 '23/12/2021
-'branch sigafyc
+'branch sigafy
 Module modMain
     Private moSesionActiva As SesionActiva
     Private moBitacoraProceso As BitacoraProceso
